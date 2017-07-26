6349d1b7953271f6309c810eb34ed9ff4dfa05df
micael.bergeron <micael.bergeron@solutionstlm.com>
2017-07-26 11:07:23 -0400

    adding rackup config.ru

--- app/config/environment.rb
+++ app/config/environment.rb
@@ -6,7 +6,7 @@ REDIS_HOST = ENV['REDIS_HOST'] || 'localhost'
 REDIS_PORT = ENV['REDIS_PORT'] || 6379
 INPUT_FILE = ENV['INPUT_FILE'] || '../data/cities_canada-usa.tsv'
 INDEX_FILE = ENV['INDEX_FILE'] || 'cities.idx'
-ENGINE     = ENV['ENGINE']     || 'redis'
+ENGINE     = ENV['ENGINE']     || 'all'
 DB_LOAD    = ENV['DB_LOAD'] == "1"
 DB_FLUSH   = ENV['DB_FLUSH'] == "1"
 DEBUG      = ENV['DEBUG'] == "1"
